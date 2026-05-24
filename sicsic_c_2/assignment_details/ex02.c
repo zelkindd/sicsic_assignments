4145b902e028a3a2f3d03b5713317d65767d9e77
Razel <razele@post.bgu.ac.il>
2026-05-24 17:40:15 +0300

    Defining functions find, add, print i w p commands work

---
+++ sicsic_c_2/assignment_details/ex02.c
@@ -0,0 +1,340 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+/**************************************************************************************************/
+/***************************YOU MUST REMARK IT BEFORE SUBMISSION***********************************/
+#define DEBUGON
+/***************************YOU MUST REMARK IT BEFORE SUBMISSION***********************************/
+/**************************************************************************************************/
+
+/* Submitters
+Name1: Razel Elmoznino   ID: 206004095
+Name2: Dan Zelkind       ID: 211571948
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/* structures */
+typedef struct item {
+    char* name;
+    int  id;
+    struct wlst* warehouses;
+} item;
+
+typedef struct warehouse {
+    char* location;
+    int  code;
+    struct itemlst* items;
+} warehouse;
+
+typedef struct itemlst {
+    item* data;
+    struct itemlst* next;
+} itemlst;
+
+typedef struct wlst {
+    warehouse* data;
+    struct wlst* next;
+} wlst;
+
+
+/******************************************* your's prototypes *******************************************************************************/
+
+item* find_item_by_id(itemlst* head, int id);
+warehouse* find_warehouse_by_code(wlst* head, int code);
+void add_item(itemlst** head, char* name, int id);
+void add_warehouse(wlst** head, char* name, int code);
+void print_items(itemlst* head);
+void print_warehouses(wlst* head);
+void print_error_message(int errid);
+
+/******************************************* your's functions ********************************************************************************/
+
+//Search for an item in the main list by ID
+item* find_item_by_id(itemlst* head, int id) {
+    itemlst* temp = head;
+    while (temp != NULL) {
+        if (temp->data->id == id) {
+            return temp->data; //Item found, return a pointer to it
+        }
+        temp = temp->next;
+    }
+    return NULL; //The item does not exist in the list
+}
+//Search for a warehouse in the main list by code
+warehouse* find_warehouse_by_code(wlst* head, int code) {
+    wlst* temp = head;
+    while (temp != NULL) {
+        if (temp->data->code == code) {
+            return temp->data; //Warehouse found, return a pointer to it
+        }
+        temp = temp->next;
+    }
+    return NULL; //The warehouse does not exist in the list
+}
+//Adds a new item to the main linked list
+void add_item(itemlst** head, char* name, int id) {
+    if (find_item_by_id(*head, id) != NULL) {
+        return; //Item exists, nothing added to the list
+    }
+    //Allocate memory for the item structure
+    item* new_item = (item*)malloc(sizeof(item));
+    if (new_item == NULL) {
+        print_error_message(1);
+    }
+    //Allocate memory for the item's name string
+    new_item->name = (char*)malloc(strlen(name) + 1);
+    if (new_item->name == NULL) {
+        free(new_item);
+        print_error_message(2);
+    }
+    //copy the data
+    strcpy(new_item->name, name);
+    new_item->id = id;
+    new_item->warehouses = NULL;
+    //Allocate memory for the main list node
+    itemlst* new_node = (itemlst*)malloc(sizeof(itemlst));
+    if (new_node == NULL) {
+        free(new_item->name);
+        free(new_item);
+        print_error_message(3);
+    }
+    new_node->data = new_item;
+    new_node->next = NULL;
+    //Insert the new node at the end of the main items list
+    if (*head == NULL) {
+        *head = new_node;
+    }
+    else {
+        itemlst* temp = *head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+}
+//Adds a new warehouse to the main linked list
+void add_warehouse(wlst** head, char* name, int code) {
+    if (find_warehouse_by_code(*head, code) != NULL) {
+        return; //Warehouse exists, nothing added to the list
+    }
+    //Allocate memory for the warehouse structure
+    warehouse* new_warehouse = (warehouse*)malloc(sizeof(warehouse));
+    if (new_warehouse == NULL) {
+        print_error_message(4);
+    }
+    //Allocate memory for the warehouse location string
+    new_warehouse->location = (char*)malloc(strlen(name) + 1);
+    if (new_warehouse->location == NULL) {
+        free(new_warehouse);
+        print_error_message(5);
+    }
+    //copy the data
+    strcpy(new_warehouse->location, name);
+    new_warehouse->code = code;
+    new_warehouse->items = NULL;
+    //Allocate memory for the main list node
+    wlst* new_node = (wlst*)malloc(sizeof(wlst));
+    if (new_node == NULL) {
+        free(new_warehouse->location);
+        free(new_warehouse);
+        print_error_message(6);
+    }
+    new_node->data = new_warehouse;
+    new_node->next = NULL;
+    //Insert the new node at the end of the main warehouses list
+    if (*head == NULL) {
+        *head = new_node;
+    }
+    else {
+        wlst* temp = *head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+}
+//Prints the entire list of items in the system
+void print_items(itemlst* head) {
+    printf("items LIST:\n");
+    itemlst* temp = head;
+    while (temp != NULL) {
+        if (temp->data != NULL) {
+            printf("%d:%s\n", temp->data->id, temp->data->name);
+        }
+        temp = temp->next;
+    }
+}
+void print_warehouses(wlst* head) {
+    printf("warehouse LIST:\n");
+    wlst* temp = head;
+    while (temp != NULL) {
+        if (temp->data != NULL) {
+            printf("Warehouse code %d, Warehouse name: %s\n", temp->data->code, temp->data->location);
+        }
+        temp = temp->next;
+    }
+}
+
+
+/*****************************************new objects and insert object functions******************************************************/
+
+
+
+/****************************************find Matchings and register item to Warehouse*************************************************/
+
+
+
+/********************************************uregisters objects*************************************************************************/
+
+
+
+/***********************************************printout functions***********************************************************************/
+
+
+/***************************************************free**********************************************************************************/
+
+
+/*******************************************Generate And Assign Items And Warehouses******************************************************/
+
+
+/*DO NOT TOUCH THIS FUNCTION */
+void getstring(char* buf, int length) {
+    int len;
+    //buf = fgets(buf, length, stdin);
+	scanf("%s", buf);
+    len = (int)strlen(buf);
+    if (buf[len - 1] == '\n')
+        buf[len - 1] = '\0';
+}
+
+
+/*DO NOT TOUCH THIS FUNCTION */
+
+void print_error_message(int errid) {
+	#ifdef DEBUGON
+	fprintf(stderr, "BGU ERROR %d\n", errid);
+	#else
+	fprintf(stderr, "BGU ERROR\n");	
+	#endif
+	exit(-1);
+}
+
+/*DO NOT TOUCH THIS MAIN TEMPLATE - ONLY ADD YOUR FUNCTIONS CALLS */
+int main() {
+    itemlst* items = 0;
+    wlst* warehouses = 0;
+    char  c;
+    char  buf[100];
+    int   id, num;
+	
+
+
+    do {
+        printf("Choose:\n"
+            "    i - new item\n"
+            "    w - new warehouse\n"
+            "    a - assign an item to a warehouse\n"
+            "    u - unassign an item from a warehouse(not delete!)\n"
+            "    p - print status\n"
+			"    g - generating and assigning 100 items to 10 warehouses\n"
+            "    q - quit\n");
+
+        while ((c = (char)getchar()) == '\n');
+        getchar();
+
+        switch (c) {
+        case 'i':
+            printf("Adding new item.\n");
+
+            printf("item name: ");
+            getstring(buf, 100);
+			
+
+            printf("item ID: ");
+            scanf("%d", &id);
+			
+			printf("\n Add new item: name %s item id: %d",buf, id);
+
+            add_item(&items, buf, id);
+
+            break;
+
+        case 'w':
+            printf("Adding new warehouse.\n");
+
+            printf("\n warehouse name: ");
+            getstring(buf, 100);
+
+            printf("\n warehouse code: ");
+            scanf("%d", &num);
+			
+			printf("\n Add new warehouse: name %s warehouse code: %d",buf, num);
+
+            add_warehouse(&warehouses, buf, num);
+
+            break;
+
+        case 'a':
+            printf("Assign an item to a warehouse.\n");
+
+            printf("item ID: ");
+            scanf("%d", &id);
+
+            printf("warehouse code: ");
+            scanf("%d", &num);
+
+            //your function
+
+            break;
+
+        case 'u':
+            printf("Remove an item from a warehouse.\n");
+
+            printf("item ID: ");
+            scanf("%d", &id);
+
+            printf("warehouse code: ");
+            scanf("%d", &num);
+
+           //your function
+
+            break;
+
+        case 'p':
+            printf("Printing status.\n");
+
+            print_items(items);
+            print_warehouses(warehouses);
+
+            break;
+			
+		case 'g':  // generating and assigning items and warehouses
+			printf("Generating and assigning items to warehouses\n");
+		
+			//your function
+			
+			break;
+
+        case 'q':
+            printf("Quitting...\n");
+            break;
+        }
+
+        if (c != 'q')
+            printf("\n");
+    } while (c != 'q');
+
+    //your free functions
+	return 0;
+    
+}
+
+
+
+
