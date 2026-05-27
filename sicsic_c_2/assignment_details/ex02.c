1c79897be89dda02ea530720425b88a4222e377d
zelkind <cookiecrafted10@gmail.com>
2026-05-27 13:31:59 +0300

    added generate functionality

--- sicsic_c_2/assignment_details/ex02.c
+++ sicsic_c_2/assignment_details/ex02.c
@@ -497,9 +497,29 @@ int main() {
 			
 		case 'g':  // generating and assigning items and warehouses
 			printf("Generating and assigning items to warehouses\n");
-		
-			//your function
-			
+
+			srand(1948);
+			for (int i = 0; i < 10; i++) {
+				strcpy(buf, "Warehouse");
+				buf[9] = '0' + i;
+				buf[10] = '\0';
+				add_warehouse(&warehouses, buf, i);
+			}
+			for (int i = 0; i < 100; i++) {
+				strcpy(buf, "Item");
+				if (i < 10) {
+					buf[4] = '0' + i;
+					buf[5] = '\0';
+				} else {
+					buf[4] = '0' + i / 10;
+					buf[5] = '0' + i % 10;
+					buf[6] = '\0';
+				}
+				add_item(&items, buf, i);
+				int randomCode = rand() % 10;
+				assign_item_to_warehouse(items, warehouses, i, randomCode);
+			}
+
 			break;
 
         case 'q':
